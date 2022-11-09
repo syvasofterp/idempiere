1327de70a309f4e757ac5b14800342004f086602
Syed <syed@syvasoft.com>
2022-11-09 10:34:47 +0530

    Update MProductionLine.java

--- org.adempiere.base/src/org/compiere/model/MProductionLine.java
+++ org.adempiere.base/src/org/compiere/model/MProductionLine.java
@@ -359,7 +359,7 @@ public class MProductionLine extends X_M_ProductionLine {
 				log.saveError("ParentComplete", Msg.translate(getCtx(), "M_Production_ID"));
 				return false;
 			}
-			if ( productionParent.getM_Product_ID() == getM_Product_ID() && productionParent.getProductionQty().signum() == getMovementQty().signum())
+			if ( productionParent.getM_Product_ID() == getM_Product_ID()) // && productionParent.getProductionQty().signum() == getMovementQty().signum())
 				setIsEndProduct(false);
 			else 
 				setIsEndProduct(true);
